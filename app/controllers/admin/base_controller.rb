25d9ac24158ce0273c21c2e2842658729be51a9e
azastela <andrii_zastela@yahoo.com>
2014-10-20 20:19:12 +0300

    Add login field to admin authentication form

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,13 @@
 class Admin::BaseController < ActionController::Base
   layout 'application'
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   before_filter :authenticate_admin!
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_in) { |a| a.permit(:login, :username, :email, :password, :remember_me) }
+    end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,27 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username, uniqueness: { case_sensitive: false }
+
+  attr_accessor :login
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+
+  def login=(login)
+   @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
